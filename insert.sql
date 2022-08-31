83dbe9152d5e7fdc84fc543c663a58129eb26b18
Fauzyah Hadirahma <97279376+fauzyahadirahma@users.noreply.github.com>
2022-09-01 06:42:01 +0700

    Membuat File Kedua
    Menyisipkan Data Tabel pada MySQL

--- insert.sql
+++ insert.sql
@@ -5,14 +5,27 @@ create table keluarga (
     Posisi text
 );
 
-insert into keluarga (No, Nama, Umur, Posisi) values (1, "Hadi", 58, "Ayah");
+insert into keluarga (No, Nama, Umur, Posisi) 
+              values (1, "Hadi", 58, "Ayah"), 
+                     (2, "Anik", 54, "Bunda"),
+                     (3, "Nisa", 23, "Anak Pertama"),
+                     (4, "Fau", 22, "Anak Kedua"),
+                     (5, "Izza", 13, "Anak Ketiga"),
+                     (6, "Ilfan", 12, "Anak Keempat");
+
+
 select * from keluarga;
 
 
 /** 
-output akan terlihat seperti di bawah:
+input akan terlihat seperti di bawah:
 
-No      Nama    Umur    Posisi 
-1       Hadi    58      Ayah   
+No      Nama    Umur    Posisi
+1       Hadi    58      Ayah
+2       Anik    54      Bunda
+3       Nisa    23      Anak Pertama
+4       Fau     22      Anak Kedua
+5       Izza    13      Anak Ketiga
+6       Ilfan   12      Anak Keempat
 
 **/
