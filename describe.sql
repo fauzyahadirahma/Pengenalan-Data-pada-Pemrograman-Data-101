edc6f39d7fe5dd4fcaa0d930be97c852a3a3d278
Fauzyah Hadirahma <97279376+fauzyahadirahma@users.noreply.github.com>
2022-09-01 06:18:20 +0700

    Memuat file pertama
    Membuat tabel pada MySQL

---
+++ describe.sql
@@ -0,0 +1,19 @@
+create table keluarga (
+    no int primary key,
+    nama text,
+    umur int,
+    posisi text
+);
+
+describe keluarga;
+
+
+/**
+input keluaran akan seperti di bawah
+
+Field   Type    Null    Key     Default Extra
+no      int     NO      PRI     NULL
+nama    text    YES             NULL
+umur    int     YES             NULL
+posisi  text    YES             NULL
+**/
